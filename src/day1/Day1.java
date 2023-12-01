916086b13d27e336dda583b44510df7a537e6f80
mg <mathieu.george92@gmail.com>
2023-12-01 07:00:15 +0100

    remove warnings

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class Day1 {
 
