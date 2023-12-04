9c7385e9592ffc28b1df58685205f8fb8f2c3b40
Mathieu George <mathieu.george@koeos.lan>
2023-12-04 14:00:11 +0100

    Day 4 part2

--- src/day4/Day4_Part2.java
+++ src/day4/Day4_Part2.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-public class Day4_Part1 {
+public class Day4_Part2 {
 
     public static void main(String[] args) throws IOException {
         Path inputPath = Paths.get("src/day4/input.txt");
@@ -14,24 +16,35 @@ public class Day4_Part1 {
         String[] lines = input.split("\r\n");
 
         // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        int sum = 0;
+        Map<Integer, Integer> scoreByCard = new HashMap<>();
+        Map<Integer, Integer> numberOfCards = new HashMap<>();
         for (String line : lines) {
             line = line.replaceAll("\\s+", ";");
             String[] splitNumbers = line.split(":;")[1].split(";\\|;");
             String[] winningNumbers = splitNumbers[0].split(";");
             String[] myNumbers = splitNumbers[1].split(";");
+            int cardId = Integer.parseInt(line.split(":;")[0].split(";")[1]);
+            numberOfCards.put(cardId, 1);
             Set<String> winningNumbersSet = Set.of(winningNumbers);
             int lineScore = 0;
-            int pow = 0;
             for (String myNumber : myNumbers) {
                 if (winningNumbersSet.contains(myNumber)) {
-                    lineScore = (int) Math.pow(2, pow);
-                    pow++;
+                    lineScore++;
                 }
             }
-            sum += lineScore;
+            scoreByCard.put(cardId, lineScore);
         }
 
+        for (Map.Entry<Integer, Integer> entry : scoreByCard.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+
+            for (int i = key + 1; i <= key + value; i++) {
+                numberOfCards.put(i, numberOfCards.get(i) + numberOfCards.get(key));
+            }
+        }
+
+        int sum = numberOfCards.values().stream().mapToInt(Integer::intValue).sum();
         System.out.println(sum);
     }
 }
