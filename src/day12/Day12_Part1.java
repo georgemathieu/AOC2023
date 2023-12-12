9924c649258aee3f1e6454e141782bda0a2dabe1
mg <mathieu.george92@gmail.com>
2023-12-12 08:03:54 +0100

    Day12 start

--- src/day12/Day12_Part1.java
+++ src/day12/Day12_Part1.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day12_Part1 {
+
+    public record Pair(List<Spring> springs, List<Integer> damagedGroups){}
 
 
     public static void main(String[] args) throws IOException {
@@ -14,7 +18,36 @@ public class Day12_Part1 {
         String input = Files.readString(inputPath);
         String[] lines = input.split("\r\n");
 
+        List<Pair> fullSpringList = new ArrayList<>();
+        for (String line : lines) {
+            List<Spring> springs = new ArrayList<>();
+            for (char c : line.split(" ")[0].toCharArray()) {
+                springs.add(Spring.toEnum(c));
+            }
+            List<Integer> damagedGroups = new ArrayList<>();
+            for (String weight : line.split(" ")[1].split(",")) {
+                damagedGroups.add(Integer.parseInt(weight));
+            }
+            fullSpringList.add(new Pair(springs, damagedGroups));
+        }
+
+        for (Pair pair : fullSpringList) {
+            
+        }
+
 
         System.out.println(input);
+    }
+}
+
+enum Spring {
+    Normal, Damaged, Unknown;
+
+    public static Spring toEnum(char c) {
+        return switch (c) {
+            case '#' -> Damaged;
+            case '?' -> Unknown;
+            default -> Normal;
+        };
     }
 }
